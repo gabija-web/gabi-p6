a5b2a4d778b4145511e17126681e836cb4029c30
gabija-web <gabijasmolskiene@gmail.com>
2020-06-29 22:30:16 +0100

    create sauce list example

--- app.js
+++ app.js
@@ -2,8 +2,45 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Your request was successful!' }); 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/sauces', (req, res, next) => {
+    const sauces = [
+        {
+            _id: '454dsfd5gfdgf5',
+            userId: '032519',
+            name: 'devil sauce',
+            manufacturer: 'Pekocko',
+            description: 'Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content.',
+            mainPepper: 'chili pepper',
+            imageUrl: '',
+            heat: 12,
+            likes: 5,
+            dislikes: 0,
+            usersLiked: [''],
+            usersDisliked: [''],
+        },
+        {
+            _id: '302dsfd5gfdgf5',
+            userId: '594519',
+            name: 'huracane sauce',
+            manufacturer: 'Pekocko',
+            description: 'Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content.',
+            mainPepper: 'chili pepper',
+            imageUrl: '',
+            heat: 10,
+            likes: 5,
+            dislikes: 0,
+            usersLiked: [''],
+            usersDisliked: [''],
+        },
+    ];
+    res.status(200).json(sauces);
 });
 
 module.exports = app;
