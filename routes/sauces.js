aac2fc8ca2b9672d0f693eca8c1e8a3cedae5c5b
gabija-web <gabijasmolskiene@gmail.com>
2020-06-30 22:26:56 +0100

    separated routes

---
+++ routes/sauces.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const router = express.Router();
+
+const Sauces = require('../models/sauces');
+
+//  ---------------------------------------- POST REQUEST  ---------------------------------------------------------
+router.post('/api/sauces', (req, res, next) => {
+    const sauces = new Sauces({
+      userId: req.body.userId,
+      name: req.body.name,
+      manufacturer: req.body.manufacturer,
+      description: req.body.description,
+      mainPepper: req.body.mainPepper,
+      imageUrl: req.body.imageUrl,
+      heat: req.body.heat,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+      usersLiked: req.body.usersLiked,
+      usersDisliked: req.body.usersDisliked,
+    });
+    sauces.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Sauces saved successfully'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+//   ------------------------------------------ GET SAUCES BY ID & UPDATE SAUCE -------------------------------------------------------
+router.get('/api/sauces/:id', (req, res, next) => {
+  Sauces.findOne({
+    _id: req.params.id
+  }).then(
+    (sauces) => {
+      res.status(200).json(sauces);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.put('/api/stuff/:id', (req, res, next) => {
+  const sauces = new Sauces({
+      userId: req.body.userId,
+      name: req.body.name,
+      manufacturer: req.body.manufacturer,
+      description: req.body.description,
+      mainPepper: req.body.mainPepper,
+      imageUrl: req.body.imageUrl,
+      heat: req.body.heat,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+      usersLiked: req.body.usersLiked,
+      usersDisliked: req.body.usersDisliked,
+  });
+  Sauces.updateOne({_id: req.params.id}, sauces).then(
+    () => {
+      res.status(201).json({
+        message:'Sauce update successfully'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+//  -------------------------------------- DELETE SAUCE ---------------------------
+router.delete('/api/sauces/:id', (req, res, next) => {
+Sauces.deleteOne({_id: req.params.id}).then(
+  () => {
+    res.status(200).json({
+      message: 'Sauce deleted'
+    });
+  }
+).catch(
+  (error) => {
+    res.status(400).json({
+      error: error
+    });
+  }
+);
+});
+
+// ------------------------------------------------------------- GET SAUCES LIST ----------------------------------------------
+router.get('/api/sauces', (req, res, next) => {
+  Sauces.find().then(
+      (sauce) => {
+          res.status(200).json(sauce);
+      }
+  ).catch(
+      (error) => {
+          res.status(400).json({
+              error: error
+          });
+      }
+  );
+});
+
+module.exports = router;
