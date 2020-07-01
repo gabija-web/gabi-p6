3a217675cd6caf2036183d362907fd4ef09c1e85
gabija-web <gabijasmolskiene@gmail.com>
2020-07-01 14:31:30 +0100

    implement login auth

--- controllers/user.js
+++ controllers/user.js
@@ -26,5 +26,38 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.mail}).then(
+        (user) => {
+            if (!user) {
+                return res.status(401).json({
+                    error: new Error('User nor found')
+                });
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: new Error('Incorrect password')
+                        });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'token'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error: error
+                    });
+                }
+            );
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
 };
