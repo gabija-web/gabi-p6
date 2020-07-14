2086c582d80a586dbcae331a870f3b25d03d1e1b
gabija-web <gabijasmolskiene@gmail.com>
2020-07-14 23:29:21 +0100

    fixing auth problem for signup and login

--- app.js
+++ app.js
@@ -25,9 +25,7 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  app.use(bodyParser.json());
 
   app.use('/images', express.static(path.join(__dirname, 'images')));
 
